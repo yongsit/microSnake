bb0fcabb4aa8b6b7716330fe72594c584d5ded18
Yongsit Meeprasert <ginosty@outlook.com>
2021-10-17 21:15:27 +0700

    Added delay

--- main.ts
+++ main.ts
@@ -1,55 +1,120 @@
-function moveSnake () {
-    angle = 40
+input.onButtonPressed(Button.A, function () {
+    delay += 100
+})
+function isCollision () {
     x = snake[0]
     y = snake[1]
-    snake.pop()
-    snake.pop()
-    if (input.rotation(Rotation.Roll) > angle && x < 4) {
-        x += 1
-    } else if (input.rotation(Rotation.Roll) > angle && x == 4) {
-        x = 0
-    } else {
-        if (input.rotation(Rotation.Roll) < angle * -1 && x > 0) {
-            x += -1
-        } else if (input.rotation(Rotation.Roll) < angle * -1 && x == 0) {
-            x = 4
+    collided = 0
+    for (let index = 0; index <= snake.length / 2 - 1; index++) {
+        if (index == 0) {
+            continue;
         } else {
-            if (input.rotation(Rotation.Pitch) > angle && y < 4) {
-                y += 1
-            } else if (input.rotation(Rotation.Pitch) > angle && y == 4) {
-                y = 0
-            } else {
-                if (input.rotation(Rotation.Pitch) < angle * -1 && y > 0) {
-                    y += -1
-                } else if (input.rotation(Rotation.Pitch) < angle * -1 && y == 0) {
-                    y = 4
-                } else {
-                	
-                }
+            if (x == snake[index * 2] && y == snake[index * 2 + 1]) {
+                collided = 1
+                break;
             }
         }
     }
-    snake.unshift(y)
-    snake.unshift(x)
+    return collided
 }
-function drawSnake () {
-    basic.clearScreen()
-    for (let index = 0; index <= snake.length / 2 - 1; index++) {
-        led.plot(snake[index * 2], snake[index * 2 + 1])
+function genFruit () {
+    while (true) {
+        x = randint(0, 4)
+        y = randint(0, 4)
+        collided = 0
+        for (let index = 0; index <= snake.length / 2 - 1; index++) {
+            if (x == snake[index * 2] && y == snake[index * 2 + 1]) {
+                collided = 1
+            }
+        }
+        if (collided == 0) {
+            fruit[0] = x
+            fruit[1] = y
+            break;
+        }
     }
 }
+function growSnake () {
+    if (snake[0] == fruit[0] && snake[1] == fruit[1]) {
+        game.setScore(game.score() + 1)
+        snake.push(snake[snake.length] - 2)
+        snake.push(snake[snake.length] - 2)
+        genFruit()
+    }
+}
+function moveSnake () {
+    angle = 40
+    minusAngle = angle * -1
+    if (input.rotation(Rotation.Roll) > angle || input.rotation(Rotation.Roll) < minusAngle || (input.rotation(Rotation.Pitch) > angle || input.rotation(Rotation.Pitch) < minusAngle)) {
+        x = snake[0]
+        y = snake[1]
+        snake.pop()
+        snake.pop()
+        if (input.rotation(Rotation.Roll) > angle && x < 4) {
+            x += 1
+        } else if (input.rotation(Rotation.Roll) > angle && x == 4) {
+            x = 0
+        } else {
+            if (input.rotation(Rotation.Roll) < minusAngle && x > 0) {
+                x += -1
+            } else if (input.rotation(Rotation.Roll) < minusAngle && x == 0) {
+                x = 4
+            } else {
+                if (input.rotation(Rotation.Pitch) > angle && y < 4) {
+                    y += 1
+                } else if (input.rotation(Rotation.Pitch) > angle && y == 4) {
+                    y = 0
+                } else {
+                    if (input.rotation(Rotation.Pitch) < minusAngle && y > 0) {
+                        y += -1
+                    } else if (input.rotation(Rotation.Pitch) < minusAngle && y == 0) {
+                        y = 4
+                    }
+                }
+            }
+        }
+        snake.unshift(y)
+        snake.unshift(x)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    if (delay > 200) {
+        delay += -100
+    }
+})
+function drawSnake () {
+    basic.clearScreen()
+    led.plotBrightness(fruit[0], fruit[1], 255)
+    for (let index = 0; index <= snake.length / 2 - 1; index++) {
+        if (index == 0) {
+            led.plotBrightness(snake[index * 2], snake[index * 2 + 1], 134)
+        } else {
+            led.plotBrightness(snake[index * 2], snake[index * 2 + 1], 56)
+        }
+    }
+}
+let minusAngle = 0
+let angle = 0
+let collided = 0
 let y = 0
 let x = 0
-let angle = 0
 let snake: number[] = []
-snake = [
-2,
-2,
-3,
-2
-]
+let fruit: number[] = []
+let delay = 0
+led.setDisplayMode(DisplayMode.Greyscale)
+delay = 500
+fruit = [0, 0]
+snake = [2, 2]
+genFruit()
+drawSnake()
 basic.forever(function () {
-    basic.pause(200)
+    basic.pause(delay)
+    growSnake()
     moveSnake()
-    drawSnake()
+    collided = isCollision()
+    if (collided == 0) {
+        drawSnake()
+    } else {
+        game.gameOver()
+    }
 })
